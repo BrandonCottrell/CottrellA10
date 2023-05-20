0ebba2dcc4bd20ce46935a6a67b8f4bd6c6d7cc7
Brandon Cottrell <bcottrell@my.wctc.edu>
2023-05-20 00:10:31 -0500

    Add project files.

---
+++ Models/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace EFTutorial_20024.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }   
+
+        // navigation property
+        public virtual List<Post> Posts { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,183 @@
+﻿using System.Xml.Serialization;
+using EFTutorial_20024.Models;
+
+namespace EFTutorial_20024
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice;
+            do
+            {
+                Console.WriteLine("Enter your selection:");
+                Console.WriteLine("1) Display all blogs");
+                Console.WriteLine("2) Add Blog");
+                Console.WriteLine("3) Display Post");
+                Console.WriteLine("4) Add Posts");
+                choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    // Display all blogs
+                    using (var context = new BlogContext())
+                    {
+                        var blogList = context.Blogs.ToList();
+
+                        Console.WriteLine("The blogs are: ");
+                        foreach (var blog in blogList)
+                        {
+                            Console.WriteLine($"     {blog.Name}");
+                        }
+                    }
+                } else if (choice == "2")
+                {
+                    // Add blog
+                    using (var context = new BlogContext())
+                    {
+                        Console.WriteLine("Enter a blog name");
+                        var blogName = Console.ReadLine();
+
+                        var blog = new Blog();
+                        blog.Name = blogName;
+
+                        context.Blogs.Add(blog);
+                        context.SaveChanges();
+                    }
+                } else if (choice == "3")
+                {
+                    //Display Post
+                    using (var context = new BlogContext())
+                    {
+                        Console.WriteLine("Which blogs posts would you like to see?");
+                        var blogPosts = Convert.ToInt32(Console.ReadLine());
+
+                        var postsList = context.Posts.Where(p => p.BlogId == blogPosts).ToList();
+
+                        Console.WriteLine("The Posts are:");
+                        foreach (var post in postsList)
+                        {
+                            Console.WriteLine($"Blog:     {post.Blog.Name}");
+                            Console.WriteLine($"     {post.Title}");
+                        }
+                    }
+ 
+                }
+                else if (choice == "4")
+                {
+                    // Add Post 
+                    using (var context = new BlogContext())
+                    {
+                        Console.WriteLine("Which blog?");
+                        var blogId = Console.ReadLine();
+                        Console.WriteLine("Enter a post title");
+                        var title = Console.ReadLine();
+                        Console.WriteLine("Enter post content");
+                        var content = Console.ReadLine();
+                        
+
+                        var post = new Post();
+                        post.Title = title;
+                        post.Content = content;
+                        post.BlogId = Convert.ToInt32(blogId);
+
+                        context.Posts.Add(post);
+                        context.SaveChanges();
+
+                    }
+                } else
+                {
+                    Console.WriteLine("Please enter valid selection");
+                }
+
+            } while (choice == "1" || choice == "2" || choice == "3" || choice == "4");
+
+
+
+        }
+    }
+
+
+
+//    public class test
+//    {
+        
+//            // create a blog name
+//            using (var context = new BlogContext())
+//            {
+//                Console.WriteLine("Enter a blog name");
+//                var blogName = Console.ReadLine();
+
+//    var blog = new Blog();
+//    blog.Name = blogName;
+
+//                context.Blogs.Add(blog);
+//                context.SaveChanges();
+//            }
+
+
+//// read the Blogs
+//using (var context = new BlogContext())
+//{
+//    var blogList = context.Blogs.ToList();
+
+//    Console.WriteLine("The blogs are: ");
+//    foreach (var blog in blogList)
+//    {
+//        Console.WriteLine($"     {blog.Name}");
+//    }
+//}
+
+////Update the blog
+//using (var context = new BlogContext())
+//{
+//    var blogToUpdate = context.Blogs.Where(b => b.BlogId == 1).First();
+
+//    Console.WriteLine($"Your choice is {blogToUpdate.Name}");
+//    Console.WriteLine($"What do you want the name to be?");
+//    var updatedName = Console.ReadLine();
+
+//    blogToUpdate.Name = updatedName;
+//    context.SaveChanges();
+//}
+
+//// Create a post
+//using (var context = new BlogContext())
+//{
+//    Console.WriteLine("Enter a post title");
+//    var titlet = Console.ReadLine();
+//    Console.WriteLine("Enter post content");
+//    var content = Console.ReadLine();
+//    Console.WriteLine("Which blog?");
+//    var blogId = Console.ReadLine();
+
+//    var post = new Post();
+//    post.Title = titlet;
+//    post.Content = content;
+//    post.BlogId = Convert.ToInt32(blogId);
+
+//    context.Posts.Add(post);
+//    context.SaveChanges();
+
+//}
+
+//// Read the posts
+//using (var context = new BlogContext())
+//{
+//    var postsList = context.Posts.ToList();
+
+//    Console.WriteLine("The Posts are:");
+//    foreach (var post in postsList)
+//    {
+//        Console.WriteLine($"Blog:     {post.Blog.Name}");
+//        Console.WriteLine($"     {post.Title}");
+//    }
+//}
+//    }
+}
+
+
+
+
+
+
